4bb4a0805f137c697cd24ea3739ad87639c6cc69
thejamilah <jamilahmcw@gmail.com>
2019-06-10 16:45:39 -0700

    TESTS PASS second commit

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-#This is using a default value for the method according to the test in the spec file. 
+#This is using a default value for the argument of the method according to the test file in the spec folder. 
 
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
